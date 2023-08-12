538e6255f1f75bc044cb35fb609bbb00238fd3cb
Sidhant Kumar Agrawal <sidhant21495@iiitd.ac.in>
2023-08-12 18:51:01 +0530

    hmmm

--- src/Pages/AppFeatures/AppFeatures.js
+++ src/Pages/AppFeatures/AppFeatures.js
@@ -1,6 +1,6 @@
 export default function AppFeatures() {
   return (
-    <div className="bg-gray-100 py-20 px-16">
+    <div className=" py-20 px-16">
       <div className="container mx-auto text-center">
         <h2 className="text-4xl font-bold mb-16">Discover Our Features</h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-16">
--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -1,25 +1,21 @@
 export default function Landing() {
   return (
-    <div className="relative container mx-auto mt-8 h-96">
-      {" "}
-      {/* Here, h-96 sets the height to 24rem (or 384px). Adjust this value as necessary. */}
+    <div className="relative container mx-auto mt-8 h-96 flex items-center justify-center">
       <img
         src={require("./oldlady.jpeg")}
         alt="Medical Equipment"
         className="absolute inset-0 w-full h-full object-cover object-center"
       />
-      <div className="bg-gradient-to-l from-transparent to-white p-8 rounded-lg shadow-md flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-8">
-        <div className="md:w-1/2 text-center md:text-left z-10">
-          <h1 className="text-4xl font-bold text-gray-900">
-            Transforming Healthcare Through Technology
-          </h1>
-          <p className="text-lg text-gray-700 mt-4">
-            Your trusted platform for managing and improving your health.
-          </p>
-          <blockquote className="text-xl italic text-gray-600 mt-8">
-            "Empowering healthier lives, one click at a time."
-          </blockquote>
-        </div>
+      <div className="absolute left-10 bg-gradient-to-r from-transparent to-white w-1/2 md:w-1/3 p-8 rounded-lg shadow-md flex flex-col items-start justify-start space-y-4">
+        <h1 className="text-4xl font-bold text-gray-900">
+          Transforming Healthcare Through Technology
+        </h1>
+        <p className="text-lg text-gray-700 mt-4">
+          Your trusted platform for managing and improving your health.
+        </p>
+        <blockquote className="text-xl italic text-gray-600 mt-8">
+          "Empowering healthier lives, one click at a time."
+        </blockquote>
       </div>
     </div>
   );
