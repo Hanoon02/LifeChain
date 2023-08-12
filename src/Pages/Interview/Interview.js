8ad2c6d4a71c7ce4c5807926c30bb7bedce9e9b5
Sidhant Kumar Agrawal <sidhant21495@iiitd.ac.in>
2023-08-12 19:37:25 +0530

    waste of time

--- src/Pages/Interview/Interview.js
+++ src/Pages/Interview/Interview.js
@@ -1,25 +1,30 @@
 import React from "react";
 
 export default function Interview() {
-    return (
-        <>
-            <div className={'flex flex-col justify-center py-[20px]'}>
-                <p className={'text-[40px] font-[600] text-center'}> Our Stories </p>
-                <div className={'flex justify-center pt-5 gap-5'}>
-                    <div className={'transform rotate-1 transition-transform hover:scale-105 hover:rotate-0 bg-[#f9f4f2] h-[400px] w-[400px] shadow-lg p-8'}>
-                        <p className={'h-1/2 text-[40px] font-[600]'}>I did blah blah blah blah blah</p>
-                        <p className={'h-1/2 flex items-end text-[20px] font-[300]'}>I did blah blah blah blah blah</p>
-                    </div>
-                    <div className={'transform rotate-1 transition-transform hover:scale-105 hover:rotate-0 bg-[#f9f4f2] h-[400px] w-[400px] shadow-lg p-8'}>
-                        <p className={'h-1/2 text-[40px] font-[600]'}>I did blah blah blah blah blah</p>
-                        <p className={'h-1/2 flex items-end text-[20px] font-[300]'}>I did blah blah blah blah blah</p>
-                    </div>
-                    <div className={'transform rotate-1 transition-transform hover:scale-105 hover:rotate-0 bg-[#f9f4f2] h-[400px] w-[400px] shadow-lg p-8'}>
-                        <p className={'h-1/2 text-[40px] font-[600]'}>I did blah blah blah blah blah</p>
-                        <p className={'h-1/2 flex items-end text-[20px] font-[300]'}>I did blah blah blah blah blah</p>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="container mx-auto mt-8 px-32 py-20">
+        <div className={"flex flex-col justify-center py-[50px]"}>
+          <p className={"text-[40px] font-[600] text-center"}> Our Stories </p>
+          <div className={"flex justify-center pt-5 gap-5"}>
+            <div className={"h-[400px] w-[400px] shadow-lg p-8"}>
+              <p className={"text-[40px] font-[600]"}>
+                I did blah blah blah blah blah
+              </p>
             </div>
-        </>
-    )
+            <div className={"h-[400px] w-[400px] shadow-lg p-8"}>
+              <p className={"text-[40px] font-[600]"}>
+                I did blah blah blah blah blah
+              </p>
+            </div>
+            <div className={"h-[400px] w-[400px] shadow-lg p-8"}>
+              <p className={"text-[40px] font-[600]"}>
+                I did blah blah blah blah blah
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
