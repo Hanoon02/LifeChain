041202102cfad2bba4aa3f0e8ba44cb630884512
Hanoon <90821506+Hanoon02@users.noreply.github.com>
2023-08-13 00:25:27 +0530

    update

--- src/Pages/Interview/Interview.js
+++ src/Pages/Interview/Interview.js
@@ -7,19 +7,28 @@ export default function Interview() {
         <div className={"flex flex-col justify-center py-[50px]"}>
           <p className={"text-[40px] font-[600] text-center"}> Our Stories </p>
           <div className={"flex justify-center pt-5 gap-5"}>
-            <div className={"h-[400px] w-[400px] shadow-lg p-8"}>
-              <p className={"text-[40px] font-[600]"}>
+            <div className={"bg-gradient-to-b from-[#ffc8a7] to-white h-[400px] w-[400px] shadow-lg p-8"}>
+              <p className={"flex items-start h-1/2 text-[30px] font-[600]"}>
                 I did blah blah blah blah blah
               </p>
-            </div>
-            <div className={"h-[400px] w-[400px] shadow-lg p-8"}>
-              <p className={"text-[40px] font-[600]"}>
-                I did blah blah blah blah blah
+              <p className={"flex items-end h-1/2 text-[18px] font-[400]"}>
+                Thomas thomas
               </p>
             </div>
-            <div className={"h-[400px] w-[400px] shadow-lg p-8"}>
-              <p className={"text-[40px] font-[600]"}>
+            <div className={"bg-gradient-to-b from-[#ffc8a7] to-white h-[400px] w-[400px] shadow-lg p-8"}>
+              <p className={"flex items-start h-1/2 text-[30px] font-[600]"}>
                 I did blah blah blah blah blah
+              </p>
+              <p className={"flex items-end h-1/2 text-[18px] font-[400]"}>
+                Thomas thomas
+              </p>
+            </div>
+            <div className={"bg-gradient-to-b from-[#ffc8a7] to-white h-[400px] w-[400px] shadow-lg p-8"}>
+              <p className={"flex items-start h-1/2 text-[30px] font-[600]"}>
+                I did blah blah blah blah blah
+              </p>
+              <p className={"flex items-end h-1/2 text-[18px] font-[400]"}>
+                Thomas thomas
               </p>
             </div>
           </div>
