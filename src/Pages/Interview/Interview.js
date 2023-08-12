6348feb44c17a4a9487401fee3205d5c48991a06
Hanoon <90821506+Hanoon02@users.noreply.github.com>
2023-08-12 19:14:55 +0530

    update

--- src/Pages/Interview/Interview.js
+++ src/Pages/Interview/Interview.js
@@ -6,15 +6,15 @@ export default function Interview() {
             <div className={'flex flex-col justify-center py-[50px]'}>
                 <p className={'text-[40px] font-[600] text-center'}> Our Stories </p>
                 <div className={'flex justify-center pt-5 gap-5'}>
-                    <div className={'bg-[#f9f4f2] h-[400px] w-[400px] shadow-lg p-8'}>
+                    <div className={'transform rotate-1 transition-transform hover:scale-105 hover:rotate-0 bg-[#f9f4f2] h-[400px] w-[400px] shadow-lg p-8'}>
                         <p className={'h-1/2 text-[40px] font-[600]'}>I did blah blah blah blah blah</p>
                         <p className={'h-1/2 flex items-end text-[20px] font-[300]'}>I did blah blah blah blah blah</p>
                     </div>
-                    <div className={'bg-[#f9f4f2] h-[400px] w-[400px] shadow-lg p-8'}>
+                    <div className={'transform rotate-1 transition-transform hover:scale-105 hover:rotate-0 bg-[#f9f4f2] h-[400px] w-[400px] shadow-lg p-8'}>
                         <p className={'h-1/2 text-[40px] font-[600]'}>I did blah blah blah blah blah</p>
                         <p className={'h-1/2 flex items-end text-[20px] font-[300]'}>I did blah blah blah blah blah</p>
                     </div>
-                    <div className={'bg-[#f9f4f2] h-[400px] w-[400px] shadow-lg p-8'}>
+                    <div className={'transform rotate-1 transition-transform hover:scale-105 hover:rotate-0 bg-[#f9f4f2] h-[400px] w-[400px] shadow-lg p-8'}>
                         <p className={'h-1/2 text-[40px] font-[600]'}>I did blah blah blah blah blah</p>
                         <p className={'h-1/2 flex items-end text-[20px] font-[300]'}>I did blah blah blah blah blah</p>
                     </div>
