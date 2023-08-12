45753c09e8c9745939da68a38d2212f80bf2f4d6
Hanoon <90821506+Hanoon02@users.noreply.github.com>
2023-08-12 19:05:32 +0530

    update

--- src/Pages/Interview/Interview.js
+++ src/Pages/Interview/Interview.js
@@ -6,14 +6,17 @@ export default function Interview() {
             <div className={'flex flex-col justify-center py-[50px]'}>
                 <p className={'text-[40px] font-[600] text-center'}> Our Stories </p>
                 <div className={'flex justify-center pt-5 gap-5'}>
-                    <div className={'h-[400px] w-[400px] shadow-lg p-8'}>
-                        <p className={'text-[40px] font-[600]'}>I did blah blah blah blah blah</p>
+                    <div className={'bg-[#f9f4f2] h-[400px] w-[400px] shadow-lg p-8'}>
+                        <p className={'h-1/2 text-[40px] font-[600]'}>I did blah blah blah blah blah</p>
+                        <p className={'h-1/2 flex items-end text-[20px] font-[300]'}>I did blah blah blah blah blah</p>
                     </div>
-                    <div className={'h-[400px] w-[400px] shadow-lg p-8'}>
-                        <p className={'text-[40px] font-[600]'}>I did blah blah blah blah blah</p>
+                    <div className={'bg-[#f9f4f2] h-[400px] w-[400px] shadow-lg p-8'}>
+                        <p className={'h-1/2 text-[40px] font-[600]'}>I did blah blah blah blah blah</p>
+                        <p className={'h-1/2 flex items-end text-[20px] font-[300]'}>I did blah blah blah blah blah</p>
                     </div>
-                    <div className={'h-[400px] w-[400px] shadow-lg p-8'}>
-                        <p className={'text-[40px] font-[600]'}>I did blah blah blah blah blah</p>
+                    <div className={'bg-[#f9f4f2] h-[400px] w-[400px] shadow-lg p-8'}>
+                        <p className={'h-1/2 text-[40px] font-[600]'}>I did blah blah blah blah blah</p>
+                        <p className={'h-1/2 flex items-end text-[20px] font-[300]'}>I did blah blah blah blah blah</p>
                     </div>
                 </div>
             </div>
