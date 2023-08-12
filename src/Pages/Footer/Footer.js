c7ef4ad108ee7a4be9d7516410192f7652c7e7dd
Hanoon <90821506+Hanoon02@users.noreply.github.com>
2023-08-12 19:33:43 +0530

    update

--- src/Pages/Footer/Footer.js
+++ src/Pages/Footer/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800 text-white py-10">
+    <footer className="bg-gray-800 text-white py-5">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div>
@@ -50,12 +50,6 @@ export default function Footer() {
               </a>
             </div>
           </div>
-        </div>
-        <div className="mt-8 border-t border-gray-700 pt-8 text-center">
-          <p>
-            &copy; {new Date().getFullYear()} YourCompanyName. All rights
-            reserved.
-          </p>
         </div>
       </div>
     </footer>
