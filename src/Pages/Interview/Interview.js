1ba7a8b2d8286e58cedbb488c793e647a1f32a17
Hanoon <90821506+Hanoon02@users.noreply.github.com>
2023-08-13 02:02:04 +0530

    update

--- src/Pages/Interview/Interview.js
+++ src/Pages/Interview/Interview.js
@@ -1,92 +1,73 @@
-// import React from "react";
+import React, {useState} from "react";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+const Testimonial = ({ content, author }) => {
+    return (
+        <div className="bg-white border p-8 h-96 w-96 shadow-lg rounded transform hover:scale-105 transition-transform border-deep-cerulean-lighter">
+            <blockquote className="flex flex-col justify-center h-3/4 text-2xl font-semibold text-deep-cerulean">
+                “{content}”
+            </blockquote>
+            <p className="flex items-end h-1/4 text-xl font-light text-deep-cerulean mt-6">
+                - {author}
+            </p>
+        </div>
+    );
+}
 
-// export default function Interview() {
-//   return (
-//     <>
-//       <div className="container mx-auto mt-8 px-32 py-20">
-//         <div className={"flex flex-col justify-center py-[50px]"}>
-//           <p className={"text-[40px] font-[600] text-center"}> Our Stories </p>
-//           <div className={"flex justify-center pt-5 gap-5"}>
-//             <div
-//               className={
-//                 "bg-gradient-to-b from-[#ffc8a7] to-white h-[400px] w-[400px] shadow-lg p-8"
-//               }
-//             >
-//               <p className={"flex items-start h-1/2 text-[30px] font-[600]"}>
-//                 I did blah blah blah blah blah
-//               </p>
-//               <p className={"flex items-end h-1/2 text-[18px] font-[400]"}>
-//                 Thomas thomas
-//               </p>
-//             </div>
-//             <div
-//               className={
-//                 "bg-gradient-to-b from-[#ffc8a7] to-white h-[400px] w-[400px] shadow-lg p-8"
-//               }
-//             >
-//               <p className={"flex items-start h-1/2 text-[30px] font-[600]"}>
-//                 I did blah blah blah blah blah
-//               </p>
-//               <p className={"flex items-end h-1/2 text-[18px] font-[400]"}>
-//                 Thomas thomas
-//               </p>
-//             </div>
-//             <div
-//               className={
-//                 "bg-gradient-to-b from-[#ffc8a7] to-white h-[400px] w-[400px] shadow-lg p-8"
-//               }
-//             >
-//               <p className={"flex items-start h-1/2 text-[30px] font-[600]"}>
-//                 I did blah blah blah blah blah
-//               </p>
-//               <p className={"flex items-end h-1/2 text-[18px] font-[400]"}>
-//                 Thomas thomas
-//               </p>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-import React from "react";
+
+const listOfTestimonials = [
+    <Testimonial
+        content="I did blah blah blah blah blah"
+        author="Thomas Thomas"
+    />,
+    <Testimonial
+        content="I felt the impact immediately in my life."
+        author="Jane Doe"
+    />,
+    <Testimonial
+        content="A revolutionary approach to managing my health."
+        author="Sam Smith"
+    />,
+    <Testimonial
+        content="Ching chong ping pong ho chih minh choo choo train"
+        author="Ching Chong"
+    />,
+    <Testimonial
+        content="Meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow meow"
+        author="Caterson Cat"
+    />
+];
+
 
 export default function Interview() {
+   const [currentTestimonial, setCurrentTestimonial] = useState(1);
+
+   const slideLeft = () => {
+         setCurrentTestimonial(currentTestimonial === 0 ? listOfTestimonials.length - 1 : currentTestimonial - 1);
+   }
+
+    const slideRight = () => {
+        setCurrentTestimonial(currentTestimonial === listOfTestimonials.length - 1 ? 0 : currentTestimonial + 1);
+    }
+
   return (
-    <div className="container mx-auto mt-8 px-32 py-20 bg-white">
+    <div className=" mx-auto mt-8 py-20 bg-white">
       <div className="flex flex-col justify-center py-12">
         <h2 className="text-4xl font-bold text-deep-cerulean-dark text-center">
           Our Stories
         </h2>
 
-        <div className="flex justify-center pt-10 gap-10">
-          <Testimonial
-            content="I did blah blah blah blah blah"
-            author="Thomas Thomas"
-          />
-          <Testimonial
-            content="I felt the impact immediately in my life."
-            author="Jane Doe"
-          />
-          <Testimonial
-            content="A revolutionary approach to managing my health."
-            author="Sam Smith"
-          />
+        <div className="w-full flex justify-center pt-10 gap-10">
+            {currentTestimonial === 0 ? listOfTestimonials[listOfTestimonials.length - 1] : listOfTestimonials[currentTestimonial-1]}
+            {listOfTestimonials[currentTestimonial]}
+            {currentTestimonial === listOfTestimonials.length - 1 ? listOfTestimonials[0] : listOfTestimonials[currentTestimonial+1]}
         </div>
+      <div className={'flex w-full justify-end pt-5 gap-1 pr-[120px]'}>
+          <div onClick={()=>slideLeft()}><ArrowBackIosNewIcon/></div>
+          <div onClick={()=>slideRight()}><ArrowForwardIosIcon/></div>
+      </div>
       </div>
     </div>
   );
 }
 
-const Testimonial = ({ content, author }) => {
-  return (
-    <div className="bg-white border p-8 h-96 w-96 shadow-lg rounded transform hover:scale-105 transition-transform border-deep-cerulean-lighter">
-      <blockquote className="flex flex-col justify-center h-3/4 text-2xl font-semibold text-deep-cerulean">
-        “{content}”
-      </blockquote>
-      <p className="flex items-end h-1/4 text-xl font-light text-deep-cerulean mt-6">
-        - {author}
-      </p>
-    </div>
-  );
-};
