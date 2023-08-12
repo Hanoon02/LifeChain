2dd23ef37da5a16028488deeeea1176202f28def
Sidhant Kumar Agrawal <sidhant21495@iiitd.ac.in>
2023-08-12 19:01:56 +0530

    apna kaam done

--- src/Pages/Interview/Interview.js
+++ src/Pages/Interview/Interview.js
@@ -1,22 +1,30 @@
 import React from "react";
 
 export default function Interview() {
-    return (
-        <>
-            <div className={'flex flex-col justify-center py-[50px]'}>
-                <p className={'text-[40px] font-[600] text-center'}> Our Stories </p>
-                <div className={'flex justify-center pt-5 gap-5'}>
-                    <div className={'h-[400px] w-[400px] shadow-lg p-8'}>
-                        <p className={'text-[40px] font-[600]'}>I did blah blah blah blah blah</p>
-                    </div>
-                    <div className={'h-[400px] w-[400px] shadow-lg p-8'}>
-                        <p className={'text-[40px] font-[600]'}>I did blah blah blah blah blah</p>
-                    </div>
-                    <div className={'h-[400px] w-[400px] shadow-lg p-8'}>
-                        <p className={'text-[40px] font-[600]'}>I did blah blah blah blah blah</p>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="container mx-auto mt-8 px-32 py-20">
+        <div className={"flex flex-col justify-center py-[50px]"}>
+          <p className={"text-[40px] font-[600] text-center"}> Our Stories </p>
+          <div className={"flex justify-center pt-5 gap-5"}>
+            <div className={"h-[400px] w-[400px] shadow-lg p-8"}>
+              <p className={"text-[40px] font-[600]"}>
+                I did blah blah blah blah blah
+              </p>
             </div>
-        </>
-    )
+            <div className={"h-[400px] w-[400px] shadow-lg p-8"}>
+              <p className={"text-[40px] font-[600]"}>
+                I did blah blah blah blah blah
+              </p>
+            </div>
+            <div className={"h-[400px] w-[400px] shadow-lg p-8"}>
+              <p className={"text-[40px] font-[600]"}>
+                I did blah blah blah blah blah
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
