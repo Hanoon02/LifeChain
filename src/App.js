2053cf03c51ceb09a13c48e94839500bbafc1372
Sidhant Kumar Agrawal <sidhant21495@iiitd.ac.in>
2023-08-12 17:08:07 +0530

    Error update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import Landing from "./Pages/Landing/Landing";
 import NavBar from "./Pages/NavBar/NavBar";
 import Slideshow from "./Pages/Slideshow/Slideshow";
 import Features from "./Pages/Features/Features";
+import Footer from "./Pages/Footer/Footer";
+import AppFeatures from "./Pages/AppFeatures/AppFeatures";
 
 function App() {
   return (
     <div className="App min-h-screen">
-        <NavBar />
-        <Landing />
-        <Features/>
-        <Slideshow />
+      <NavBar />
+      <Landing />
+      <AppFeatures />
+      <Features />
+      <Slideshow />
+      <Footer />
     </div>
   );
 }
