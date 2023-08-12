fd1314f289f67570ad911c3f3ad5e162c0c9a1f1
Hanoon <90821506+Hanoon02@users.noreply.github.com>
2023-08-12 17:50:31 +0530

    update

--- src/Pages/Interview/Interview.js
+++ src/Pages/Interview/Interview.js
@@ -4,9 +4,15 @@ export default function Interview() {
     return (
         <>
             <div>
-                <p className={'text-[40px] text-center font-[400]'}> Our Stories </p>
-                <div className={'flex justify-center'}>
-                    <div className={'h-[300px] w-1/3'}>
+                <p className={'text-[40px] text-center font-[600] py-[50px]'}> Our Stories </p>
+                <div className={'flex justify-center pt-5 gap-5'}>
+                    <div className={'h-[400px] w-[400px] shadow-lg'}>
+
+                    </div>
+                    <div className={'h-[400px] w-[400px] shadow-lg'}>
+
+                    </div>
+                    <div className={'h-[400px] w-[400px] shadow-lg'}>
 
                     </div>
                 </div>
